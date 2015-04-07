757f93fe5dab8c115c35e86164440f104bb1d04b
cmcollin41 <cmcollin41@gmail.com>
2015-04-07 16:04:03 -0600

    added proper host to config/environments for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Devise
-  config.action_mailer.default_url_options = { 'sevasports.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'sevasports.herokuapp.com' }
 end
